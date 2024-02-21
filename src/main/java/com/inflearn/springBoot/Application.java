c639d58650cfe4b37e1313082da933e550de4c23
jeongahkim <jeongah1321@gmail.com>
2024-02-21 18:48:04 +0900

    @EnableAutoConfiguration 없이도 spring 구동 가능

--- src/main/java/com/inflearn/springBoot/Application.java
+++ src/main/java/com/inflearn/springBoot/Application.java
@@ -3,8 +3,8 @@ package com.inflearn.springBoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 //@SpringBootApplication
@@ -16,6 +16,7 @@ public class Application {
         SpringApplication application = new SpringApplication(Application.class);
         application.setWebApplicationType(WebApplicationType.NONE);
         application.run(args);
+
         // step1 started
         //SpringApplication.run(Application.class, args);
     }
